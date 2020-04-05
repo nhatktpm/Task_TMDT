ec6a427a33dc4b0405d00783029a4ea8728ad2c3
=nguyen truong nhat <=nhatmytu@gmail.com>
2020-04-05 08:20:41 +0700

    test

---
+++ detail.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Detail</title>
+<meta charshet="utf-8">
+<link rel="stylesheet" href="detail.css">
+</head>
+<body>
+    <div id="menu">
+        HÃY Ở NHÀ
+    </div>
+
+    <div id="top">
+        <ul>
+            <li><a href="">Trang chủ</a></li>
+            <li><a class="active" href="">Sản phẩm</a></li>
+            <li><a href="">Giỏ hàng</a></li>
+            <li><a href="">Đơn hàng</a></li>
+            <li><a href="">Liên hệ</a></li>
+        </ul>
+    </div>
+
+    <div class="clr"></div>
+    <div>      
+        <div id="cart">cart</div>
+        <div id="glass">
+         <img src="img/kin.png" alt="hinhloi"></div>
+        <div id="find">
+            <input type="search" id="search" placeholder="Search..." />
+        </div>
+    </div>
+    <div class="clr"></div>
+    <div id="logo">
+        HOODIE
+    </div>
+    
+
+    <div id= "container">
+        <div id="category">
+            <ul>
+                <li> <a href="#">                    
+                        <img src="ktpm.png" alt="aaa">                    
+                    </a>
+                </li>
+                <br>
+                <li> <img class="a" src="img/h1a.png" alt=""></li> 
+                <li> <img class="a" src="img/h1b.png" alt=""></li>
+                
+                
+            </ul>
+        </div>
+
+        </div>
+        <div id="img-pro">
+              <a class="hinh" href="#">
+                  <img src="img/h1a.png" alt="hinhloi">
+              </a>
+              <a  class="hinh" href="#">
+                  <img src="img/h1b.png" alt="hinhloi">
+              </a>
+        </div>
+        <div id="content">
+            <div id="inf">
+                <strong> DVRK LOVERS JACKET - BLACK</strong>
+                <hr>
+                <b>Giá</b>
+                <p>80000đ</p>
+                <label for="size">SIZE</label><br>
+                    <select id="size">
+                        <option value="s">S</option>
+                        <option value="l">L</option>
+                        <option value="m">M</option>
+                        <option value="xl">XL</option>
+                    </select>                    
+                    <br><br>
+                <b>Mô Tả</b>
+                <div>
+                    <p>Ví dụ như phần tử có đường viền màu xanh bên phải, tôi thiết lập cho nó thuộc tín</p>
+                </div>
+            </div>
+            <div id=add>
+            <button class="button btnadd">Thêm Vào Giỏ Hàng</button>
+            </div>
+        </div>
+    </div>
+    <div class="clr"></div>
+    <hr>
+    <footer>
+        <div id="ft1">
+           <span> KTPM SHOP UY TÍN-CHẤT LƯỢNG-HÔI NÁCH TỎA RA PHONG CÁCH </span> 
+        </div> 
+        <div id="ft2">
+            <ul>
+                <div class="bot" >
+                    <li>
+                        <img src="hoantien.png" alt="hinhloi">
+                     </li>
+                    <span class="pan">Hoàn Tiền</span>
+                    <div class="clr"></div>                    
+                    <span>nếu sản phẩm lỗi</span>
+                </div>
+
+                <div class="bot">
+                    <li>
+                        <img src="vanchuyen.png" alt="hinhloi">
+                     </li>                     
+                    <span class="pan">Miễn Phí </span>
+                    <div class="clr"></div>                    
+                    <span>vận chuyễn toàn quốc</span>
+                </div>
+
+                <div class="bot">
+                    <li>
+                        <img src="hoi.png" alt="hinhloi">
+                     </li>
+                    <span class="pan">Tư Vấn</span>
+                    <div class="clr"></div>                    
+                    <span>mọi luc mọi nơi</span>
+                </div>
+            </ul>
+        </div>   
+    </footer>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Products</title>
+<meta charshet="utf-8">
+<link rel="stylesheet" href="products.css">
+</head>
+
+<body>
+    <div id="top">
+        <ul>
+            <li>Trang chủ</li>
+            <li>Dăng nhập</li>            
+        </ul>                
+    </div>
+    <div id="container">
+
+        <div id="category">
+            <ul>
+                <li> <a href="#">
+                    
+                        <img src="dvrk.png" alt="aaa">
+                    
+                    </a>
+                 </li>
+                <li> <a class="a" href="#">SHIRT</a></li> 
+                <li> <a class="a"href="#">T SHIRT</a></li>
+                <li> <a class="a" href="#">HOODIE</a></li>
+                <li> <a class="a" href="#">BAG</a></li>
+                         
+                
+            </ul>
+        </div>
+        
+        <div id="content">
+            <div id="logo">
+                <p>HOODIE</p>                                            
+            </div>
+            
+            <div class="clr"></div>
+            <div id="products">
+                <?php
+                for ($i = 1; $i <= 30; $i++){                       
+                ?>
+                    <div class="pro" id="<?php echo $i ?>" >                    
+                         <div class="img-pro" >
+                            <br>
+                                <a href="#">
+                                  <img src="dvrk.png" alt="#"> 
+                                </a>   
+                            <br>
+                         </div> 
+                         
+                         <div class="tenao">
+                             <strong>
+                                <?php echo "Áo Số $i" ?>
+                             </strong>                         
+                         </div>                             
+                    </div>                      
+                <?php                   
+                    }
+                ?>                 
+                           
+            </div> 
+                   
+        </div>  
+    </div>     
+        
+    <img src="img/h1.png" alt="adasd">  
+</body>
+</html>
